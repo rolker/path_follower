b6b7941bec45af8a50bc5b1b9ec21e2a778a82a0
Roland Arsenault <roland@ccom.unh.edu>
2021-04-16 17:30:16 -0400

    Use map frame from params for transform lookups

--- src/mpc_adapter_node.cpp
+++ src/mpc_adapter_node.cpp
@@ -57,13 +57,13 @@ public:
 
     bool updateReferenceTrajectory(path_planner_common::UpdateReferenceTrajectory::Request &req, path_planner_common::UpdateReferenceTrajectory::Response &res)
     {
-        ROS_INFO_STREAM("Got " << req.plan.paths.size() << " dubins paths");
+        ROS_DEBUG_STREAM("Got " << req.plan.paths.size() << " dubins paths");
         
         if(!req.plan.paths.empty())
         {
             while (!m_transformations().canTransform(m_map_frame, "earth", ros::Time(0), ros::Duration(0.5)))
             {
-                ROS_INFO("Waiting for origin...");
+                ROS_WARN("Waiting for origin...");
             }
 
             path_follower::path_followerGoal goal;
@@ -82,20 +82,20 @@ public:
                 dpath.param[2] = path.length2;
                 dpath.rho = path.rho;
                 dpath.type = DubinsPathType(path.type);
-                ROS_INFO_STREAM("path lengths: " << path.length0 << ", " << path.length1 << ", " << path.length2);
+                ROS_DEBUG_STREAM("path lengths: " << path.length0 << ", " << path.length1 << ", " << path.length2);
                 
                 int ret = dubins_path_sample_many(&dpath, goal.speed, buildPath, &states);
             }
             
             if(states.size() > 1 && m_odometry)
             {
-                ROS_INFO_STREAM("path first point: " << states[0].x << ", " << states[0].y << " yaw: " << states[0].yaw  << " (degs) " << states[0].yaw*180/M_PI);
-                ROS_INFO_STREAM("path second point: " << states[1].x << ", " << states[1].y << " yaw: " << states[1].yaw  << " (degs) " << states[1].yaw*180/M_PI);
+                ROS_DEBUG_STREAM("path first point: " << states[0].x << ", " << states[0].y << " yaw: " << states[0].yaw  << " (degs) " << states[0].yaw*180/M_PI);
+                ROS_DEBUG_STREAM("path second point: " << states[1].x << ", " << states[1].y << " yaw: " << states[1].yaw  << " (degs) " << states[1].yaw*180/M_PI);
                 
-                ROS_INFO_STREAM("now:\n" << *m_odometry);
+                ROS_DEBUG_STREAM("now:\n" << *m_odometry);
                 double next_x = m_odometry->pose.pose.position.x + m_odometry->twist.twist.linear.x;
                 double next_y = m_odometry->pose.pose.position.y + m_odometry->twist.twist.linear.y;
-                ROS_INFO_STREAM("in a sec: " << next_x << ", " << next_y);
+                ROS_DEBUG_STREAM("in a sec: " << next_x << ", " << next_y);
                 res.state.x = next_x;
                 res.state.y = next_y;
                 res.state.heading = M_PI_2-tf2::getYaw(m_odometry->pose.pose.orientation);
@@ -117,7 +117,7 @@ public:
                     point.x = s.x;
                     point.y = s.y;
                     
-                    auto position = m_transformations.map_to_wgs84(point);
+                    auto position = m_transformations.map_to_wgs84(point, m_map_frame);
 
                     geographic_msgs::GeoPoseStamped gps;
                     gps.pose.position.latitude = position.latitude;
